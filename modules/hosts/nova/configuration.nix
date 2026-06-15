62845dbd1b5f9125c48b78fa27b38684e8de8ad0
Janek Waugh <janekwaugh81@gmail.com>
2026-06-14 23:38:33 -0500

    Configured system config

--- modules/hosts/nova/configuration.nix
+++ modules/hosts/nova/configuration.nix
@@ -14,8 +14,38 @@
         # Disko
         inputs.disko.nixosModules.disko
         self.diskoConfigurations.hostNova
-
       ];
+
+      networking = {
+        hostName = "nova";
+        networkmanager.enable = true;
+      };
+
+      time.timeZone = "America/Jamaica";
+
+      boot = {
+        loader.systemd-boot.enable = true;
+        loader.efi.canTouchEfiVariables = true;
+
+        kernelPackages = pkgs.linuxPackages_latest;
+      };
+
+      users.users.kyle = {
+        isNormalUser = true;
+        shell = "/etc/profiles/per-user/kyle/bin/zsh";
+        extraGroups = [
+          "wheel"
+          "input"
+          "audio"
+          "network"
+        ];
+      };
+
+      zramSwap = {
+        enable = true;
+        algorithm = "zstd";
+        memoryPercent = 50;
+      };
     };
   };
 }
