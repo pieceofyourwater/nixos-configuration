cfe87f624c5f3cf4734a4bce0cc53322385012c2
Janek Waugh <janekwaugh81@gmail.com>
2026-06-17 19:48:31 -0500

    FIx

---
+++ modules/disko.nix
@@ -0,0 +1,62 @@
+{ self, ... }:
+{
+  flake.diskoConfigurations.default = self.diskoConfigurations.hostNova;
+  flake.diskoConfigurations.hostNova = {
+    disko.devices = {
+      disk = {
+        main = {
+          device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLB256HAHQ-000L2_S41FNE0M430866";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                priority = 1;
+                name = "ESP";
+                start = "1M";
+                end = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [ "umask=0077" ];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ]; 
+                  subvolumes = {
+                    "@" = {
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                      mountpoint = "/";
+                    };
+                    "@home" = {
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                      mountpoint = "/home";
+                    };
+                    "@nix" = {
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                      mountpoint = "/nix";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
