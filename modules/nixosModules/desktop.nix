fe3417e0e6938798c2e98d98944e09bbbb19356c
Kyle Howell <libkyle@proton.me>
2026-06-18 00:28:44 -0500

    Disabled xterm

--- modules/nixosModules/desktop.nix
+++ modules/nixosModules/desktop.nix
@@ -7,6 +7,7 @@
     };
 
     services.xserver.enable = true;
+    services.xserver.xterm.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
